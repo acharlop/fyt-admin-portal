4060182aa8de91c4e91d2f50f04a0842f3aee035
Avi Charlop <avicharlop@gmail.com>
2022-06-01 19:58:59 +0200

    add prettier, eslint, chakra ui with theming and provider

---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+// pages/_document.js
+
+import { CSSReset, ColorModeScript } from '@chakra-ui/react'
+import NextDocument, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
+
+import theme from '#/styles/theme'
+
+export default class Document extends NextDocument {
+	static async getInitialProps(ctx: DocumentContext) {
+    return NextDocument.getInitialProps(ctx)
+  }
+
+  render() {
+    return (
+      <Html lang='en'>
+        <Head />
+        <body>
+          <CSSReset />
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} type='localStorage' />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
---
+++ src/ThemeToggle.tsx
@@ -0,0 +1,14 @@
+import { Button, useColorMode } from '@chakra-ui/react'
+
+const ThemeToggle = () => {
+  const { colorMode, toggleColorMode } = useColorMode()
+  return (
+    <header>
+      <Button onClick={toggleColorMode}>
+        Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
+      </Button>
+    </header>
+  )
+}
+
+export default ThemeToggle
