10548bc872f028cb0394ab19bcfe347bda3a4046
Avi Charlop <avicharlop@gmail.com>
2022-06-02 15:53:09 +0200

    login page with working form

---
+++ components/layout/Head.tsx
@@ -0,0 +1,17 @@
+import NextHead from 'next/head'
+
+const Head = () => (
+  <NextHead>
+    <title>Login</title>
+    <meta
+      name='description'
+      content='Login to admin page'
+    />
+    <link
+      rel='icon'
+      href='/favicon.ico'
+    />
+  </NextHead>
+)
+
+export default Head
